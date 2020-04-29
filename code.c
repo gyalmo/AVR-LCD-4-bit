45418c5c621c972c2645cef614d1e8e038f4014a
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2020-04-29 15:06:24 +0530

    Update code.c

--- code.c
+++ code.c
@@ -4,7 +4,8 @@ void trigger();
 void display(unsigned char ch);
 void cmd(unsigned char ch);
 int main(void) 
-{ DDRD=0xff;
+{
+  DDRD=0xff;
   cmd(0x02);
   cmd(0x28);
   cmd(0x0f);
@@ -12,13 +13,16 @@ int main(void)
   unsigned char line_1[]="ABC DE";
   unsigned char line_2[]="Fbcdefghijk";
   for(unsigned int i=0,j=0;line_1[i]!='\0'||line_2[j]!='\0';) 
-    { if(line_1[i]!='\0') 
-       { cmd(0x85+i);
+    { 
+      if(line_1[i]!='\0') 
+       {
+         cmd(0x85+i);
          display(line_1[i]);
          i++;
        }
       if(line_2[j]!='\0') 
-       { cmd(0xc3+j);
+       { 
+        cmd(0xc3+j);
          display(line_2[j]);
          j++;
        }
@@ -26,19 +30,22 @@ int main(void)
   while(1);
 }
 void cmd(unsigned char ch)
-{ PORTD=ch&0xf0;
+{ 
+  PORTD=ch&0xf0;
   trigger();
   PORTD=ch<<4;
   trigger();
 }
 void display(unsigned char ch)
-{ PORTD=(ch&0xf0)|1;
+{ 
+  PORTD=(ch&0xf0)|1;
   trigger();
   PORTD=(ch<<4)|1;
   trigger();
 }
 void trigger()
-{ PORTD|=(1<<2);
+{ 
+  PORTD|=(1<<2);
   _delay_ms(1);
   PORTD&=~(1<<2);
 }
